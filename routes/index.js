d82091398d10dea68b4774353d4c0b1487c71300
XHFkindergarten <791319924@qq.com>
2019-05-28 13:51:14 +0800

    5-28删除wdnmd逻辑

--- routes/index.js
+++ routes/index.js
@@ -146,28 +146,28 @@ router.get('/getQnToken', async ctx => {
   ctx.status = 400
 })
 
-/**
- * @router GET /wdnmd
- * @params openId
- * @description 用户不想选书，就给他记录一下selectd
- */
-router.get('/wdnmd', async ctx => {
-  const openId = ctx.query.openId
-  const user = await User.findOne({
-    where: {
-      openId
-    }
-  })
-  await user.update({
-    selected: 'WDNMD'
-  })
-  ctx.status = 200
-  ctx.body = {
-    success: true,
-    msg: 'wdnmd success',
-    userInfo: user
-  }
-})
+// /**
+//  * @router GET /wdnmd
+//  * @params openId
+//  * @description 用户不想选书，就给他记录一下selectd
+//  */
+// router.get('/wdnmd', async ctx => {
+//   const openId = ctx.query.openId
+//   const user = await User.findOne({
+//     where: {
+//       openId
+//     }
+//   })
+//   await user.update({
+//     selected: 'WDNMD'
+//   })
+//   ctx.status = 200
+//   ctx.body = {
+//     success: true,
+//     msg: 'wdnmd success',
+//     userInfo: user
+//   }
+// })
 
 /**
  * @router GET /mycard
